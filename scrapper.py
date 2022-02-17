22a815762e2a3620ae4a68e2f41f14ffb576e43c
Henry Fabricio Caraguay Ordonez <henryfco@MBP-de-Henry.domain.name>
2022-02-17 02:00:52 -0500

    Found how to iterate trough a set of html labels

--- scrapper.py
+++ scrapper.py
@@ -3,9 +3,23 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 
-# Dirección de la página web
-url = "http://quotes.toscrape.com/"
+# Dirección de la página web a scrapear
+url = "https://news.ycombinator.com/"
 # Ejecutar GET-Request
 response = requests.get(url)
 # Analizar sintácticamente el archivo HTML de BeautifulSoup del texto fuente
 html = BeautifulSoup(response.text, 'html.parser')
+news = html.find_all("score")
+scores = html.find_all("span", {"class": "score"})
+for score in scores:
+    print(score.string)
+
+
+
+#print(news)
+# i=0
+# while(i<30):
+#     print(html.title)
+#     i+=1
+
+
