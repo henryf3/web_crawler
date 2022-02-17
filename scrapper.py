73f1dd734b6338c8adf90e8ac8a695f269247cc9
Henry Fabricio Caraguay Ordonez <henryfco@MBP-de-Henry.domain.name>
2022-02-17 04:16:53 -0500

    Data extraction finished

--- scrapper.py
+++ scrapper.py
@@ -1,30 +1,52 @@
-# Importar módulos
+from array import array
 import requests
 import csv
 from bs4 import BeautifulSoup
 
-# Dirección de la página web a scrapear
+# Website to crawl
 url = "https://news.ycombinator.com/"
-# Ejecutar GET-Request
+
+# Execute GET-Request
 response = requests.get(url)
-# Analizar sintácticamente el archivo HTML de BeautifulSoup del texto fuente
+
+# Analyze HTML file syntax using BeautifulSoup 
 html = BeautifulSoup(response.text, 'html.parser')
-news = html.find_all("score")
-#Crear un array para ir guardando los elementos deseados
 
-scores = html.find_all("span", {"class": "score"})
-scores = html.find_all("span", {"class": "score"})
+#Get order
+orders = html.find_all("span", {"class": "rank"}) #orden
 
-print(len(scores))
-for score in scores:
-    print(score.string)
+#Get titles
+titles = html.find_all("a", {"class": "titlelink"}) #titles
 
+#Get only number of points
+points = [0]*30
+scores = html.find_all("span", {"class": "score"}) #points
+i = 0
+for score in scores: 
+    text_comm = score.string
+    #print(i)
+    #print(text_comm)
+    numbers = [int(s) for s in text_comm.split() if s.isdigit()]
+    if (len(numbers)!=0):
+        #print(numbers[0])
+        points[i]=numbers[0]
+        i+=1
 
+#Get only number of comments
+comments = [0]*30
+trs = html.find_all(class_="athing") #ids
+i=0
+for tr in trs :
+    n_com1 = html.find_all("a", {"href": "item?id="+tr.get('id')})
+    text_comm = n_com1[1].string
+    numbers = [int(s) for s in text_comm.split() if s.isdigit()]
+    if (len(numbers)!=0):
+        comments[i]=numbers[0]
+        i+=1
 
-#print(news)
-# i=0
-# while(i<30):
-#     print(html.title)
-#     i+=1
-
+for i in range(30): 
+    print(orders[i].string)
+    print(titles[i].string)
+    print(points[i])
+    print(comments[i])
 
