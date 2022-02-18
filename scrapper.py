8f04631107f7a2a012bf2b3f3c82976e09e03700
Henry Fabricio Caraguay Ordonez <henryfco@MBP-de-Henry.domain.name>
2022-02-18 07:16:43 -0500

    Added some small unit testing

--- scrapper.py
+++ scrapper.py
@@ -1,28 +1,48 @@
 from array import array
 import requests
-import csv
+import unittest
 from bs4 import BeautifulSoup
 
+response = 404
+n_positions = 0
+n_titles = 0
+
+class TestCode(unittest.TestCase):
+    #To test if the request was succesful 
+    def test_request(self):
+        self.assertEqual(response.status_code,200, "Should be 6")
+    #To test if there are 30 positions in the requested info
+    def test_positions(self):
+        self.assertEqual(n_positions,30, "Should be 30")
+    #To test if there are 30 titles in the requested info
+    def test_titles(self):
+        self.assertEqual(n_titles,30, "Should be 30")
+    
 def printresults(entries):
     for i in range(len(entries)):
         print(entries[i][0]+" "+entries[i][1]+". Points:"+str(entries[i][2])+ ". Comments: "+str(entries[i][3]))
     print("")
-    
+
 def main():
     # Website to crawl
     url = "https://news.ycombinator.com/"
 
     # Execute GET-Request
+    global response 
     response = requests.get(url)
 
     # Analyze HTML file syntax using BeautifulSoup 
     html = BeautifulSoup(response.text, 'html.parser')
 
-    #Get order
-    orders = html.find_all("span", {"class": "rank"}) #orden
+    #Get positions
+    positions = html.find_all("span", {"class": "rank"}) #positions
+    global n_positions
+    n_positions = len(positions)
 
     #Get titles
     titles = html.find_all("a", {"class": "titlelink"}) #titles
+    global n_titles
+    n_titles = len(titles)
 
     #Get only number of points
     points = [0]*30
@@ -30,6 +50,7 @@ def main():
     i = 0
     for score in scores: 
         text_comm = score.string
+        #Extracting the number found in the string
         numbers = [int(s) for s in text_comm.split() if s.isdigit()]
         #To check if the string score.string was not empty
         if (len(numbers)!=0):
@@ -49,7 +70,8 @@ def main():
                 text_comm = n_com[j].string
                 break
             else:
-                text_comm="0"
+                text_comm="0"       
+        #Extracting the number found in the string
         numbers = [int(s) for s in text_comm.split() if s.isdigit()]
         comments[i]=numbers[0]
         i+=1
@@ -57,19 +79,25 @@ def main():
     #Constructing a list of entries
     entries =[]
     for i in range(30):
-        entry = (orders[i].string, titles[i].string, points[i], comments[i])
+        entry = (positions[i].string, titles[i].string, points[i], comments[i])
         entries.append(entry)
         
     #Entries with more than five words in the title ordered by the number of comments first.
+    #Filtering entries with more than five words in the title.
     entry_set_A = list(filter(lambda n:len(n[1].split())>5,entries))
+    #Ordering previous entries by number of comments.
     entry_set_A.sort(key=lambda x:x[3])
-    print("Entries with more than five words in the title ordered by the number of comments first")
+    print("Entries with more than five words in the title ordered by the number of comments")
     printresults(entry_set_A)
 
     #Entries with less than or equal to five words in the title ordered by points.
+    #Filtering entries with less than or equal to five words in the title.
     entry_set_B = list(filter(lambda n:len(n[1].split())<=5,entries))
+    #Ordering previous entries by number of comments.
     entry_set_B.sort(key=lambda x:x[2])
     print("Entries with less than or equal to five words in the title ordered by points.")
     printresults(entry_set_B)
 
-main()
+if __name__ == '__main__':
+    main()
+    unittest.main()
